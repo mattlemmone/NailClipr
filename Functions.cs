fd7d6d41417da0c3755f2f4ad65443067e18376f
Matt Lemmone <mattlemmone@att.net>
2016-03-19 19:57:24 -0400

    Fixed speed resets.

--- Functions.cs
+++ Functions.cs
@@ -152,7 +152,7 @@ namespace NailClipr
         public static void updateTrackSpeed(System.Windows.Forms.TrackBar bar, System.Windows.Forms.Label lbl, float speed, EliteAPI api = null)
         {
             //Only update GUI speed if not in combat or CS.
-            if (api == null || ((api.Player.Status != 1 && api.Player.Status != 4 )))
+            if (api == null || (api.Player.Status != 1 && api.Player.Status != 4  && api.Player.Speed >= Structs.Speed.NATURAL))
             {
                 lbl.Text = "x" + speed / Structs.Speed.NATURAL;
 

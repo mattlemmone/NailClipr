238a89a39f510ceddbff37349ea7e7cb7273a391
Matt Lemmone <mattlemmone@att.net>
2016-03-26 01:40:42 -0400

    updater complete.

--- Classes/Structs.cs
+++ Classes/Structs.cs
@@ -15,7 +15,7 @@ namespace NailClipr
         public struct App
         {
             public static string name = "NailClipr";
-            private static int[] v = { 1, 0, 5 };
+            private static int[] v = { 1, 0, 6 };
             public static string ver = string.Join(".", v);
         }
         public struct Chat
